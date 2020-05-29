cb30ce0cc4c42bc6d3cc34ef110f5d22f0c488ff
Trey Waller <Trey@treys-mbp.attlocal.net>
2020-05-29 12:34:17 -0500

    palindrome component complete

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Palindrome() {
-    return (
-        <div>
-            <p>Palindrome</p>
-        </div>
-    )
+export default class Palindrome extends Component {
+    constructor() {
+        super();
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleInput = (e) => {
+        this.setState({userInput: e.target.value});
+    }
+
+    handleConfirmation = () => {
+        const forward = this.state.userInput;
+        const backward = this.state.userInput.split('').reverse().join('');
+        console.log(backward);
+
+        if(forward === backward) {
+            this.setState({palindrome: 'true'})
+        } else this.setState({palindrome: 'false'})
+    }
+
+    render() {
+        return (
+            <div className='puzzleBox palindromePB'>
+                <h4>Palindrome</h4>
+                <input className='inputLine' onChange={this.handleInput}/>
+                <button className='confirmationButton' onClick={this.handleConfirmation}/>
+                <span className='resultsBox'>Palindrome: {this.state.palindrome}</span>
+            </div>
+        )
+    }
 }
